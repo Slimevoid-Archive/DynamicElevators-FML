75c10db64c557e92911179356762916c49e64f8e
Eurymachus <reflex_ion@hotmail.com>
2012-11-29 22:59:13 +0000

    Formatted Core Mod File

--- common/elevators/core/DynamicElevators.java
+++ common/elevators/core/DynamicElevators.java
@@ -8,11 +8,30 @@ import cpw.mods.fml.common.network.NetworkMod;
 import elevators.network.ElevatorPacketHandler;
 import eurysmods.api.ICommonProxy;
 
-@Mod(modid = "dynamic_elevators", name = "Dynamic Elevators", version = "1.7.5", useMetadata = true)
-@NetworkMod(clientSideRequired = true, channels = { "DE_GUI_DATA", "DE_UPDATE",
-		"DE_EPROP", "DE_ERROR", "DE_SHCI" }, packetHandler = ElevatorPacketHandler.class, connectionHandler = ElevatorPacketHandler.class)
+@Mod(
+		modid = "dynamic_elevators",
+		name = "Dynamic Elevators",
+		version = "1.7.5",
+		useMetadata = true
+)
+@NetworkMod(
+		clientSideRequired = true,
+		channels = {
+				"DE_GUI_DATA",
+				"DE_UPDATE",
+				"DE_EPROP",
+				"DE_ERROR",
+				"DE_SHCI"
+				},
+		packetHandler = ElevatorPacketHandler.class,
+		connectionHandler = ElevatorPacketHandler.class
+)
+
 public class DynamicElevators {
-	@SidedProxy(clientSide = "elevators.proxy.DE_ClientProxy", serverSide = "elevators.proxy.DE_CommonProxy")
+	@SidedProxy(
+			clientSide = "elevators.proxy.DE_ClientProxy",
+			serverSide = "elevators.proxy.DE_CommonProxy"
+	)
 	public static ICommonProxy proxy;
 
 	public static Object instance;
