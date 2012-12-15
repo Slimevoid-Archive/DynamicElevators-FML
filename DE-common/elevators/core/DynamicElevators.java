909b03fc382cdd76d7eb024e0e91f1ac158932ff
Eurymachus <reflex_ion@hotmail.com>
2012-12-15 16:05:26 +0000

    Updated to correct version

--- DE-common/elevators/core/DynamicElevators.java
+++ DE-common/elevators/core/DynamicElevators.java
@@ -12,7 +12,7 @@ import eurysmods.api.ICommonProxy;
 @Mod(
 		modid = "DynamicElevators",
 		name = "Dynamic Elevators",
-		version = "2.0.0.1",
+		version = "2.0.0.2",
 		useMetadata = true,
 		dependencies = "after:EurysCore")
 @NetworkMod(
